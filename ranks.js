b29a3ce18620fc0ce9330fd9f08c507980458b66
A Thomas Bianchi <atbianchi@gmail.com>
2020-08-05 22:39:48 -0400

    parse ranks

--- ranks.js
+++ ranks.js
@@ -11,27 +11,66 @@ teams.forEach(team => ranks.push({
   pointsFor: 0,
   pointsAgainst: 0,
 }))
-console.log(ranks);
+const nascarRanks = ranks.map(team => {return {...team}})
 
-let weeks = Object.keys(resultsJSON).filter(week => (Object.values(resultsJSON[week]).reduce((a,b) => a + b)) > 0);
+const weeks = Object.keys(resultsJSON).filter(week => (Object.values(resultsJSON[week]).reduce((a,b) => a + b)) > 0).slice(0,1);
+
+const updateRanks = (rankObj, points, opponent, week) => {
+  let oppPoints = resultsJSON[week][opponent];
+  if (points > oppPoints) rankObj.wins += 1;
+  if (oppPoints > points) rankObj.losses += 1;
+  if (points === oppPoints) rankObj.ties += 1;
+  rankObj.pointsFor += points;
+  rankObj.pointsAgainst += oppPoints;
+}
 
 weeks.forEach(week => {
   teams.forEach(team => {
     let points = resultsJSON[week][team];
+    
     let pod = schedule[week].find(pod => pod.includes(team)).filter(x => x !== team);
-    console.log(week, team, points);
-    console.log(pod);
+    let nascarPod = teams.filter(x => x !== team);
+    // console.log(nascarPod.length);
+    
+    let rankObj = ranks.find(rank => rank.team === team);
+    let nascarObj = nascarRanks.find(rank => rank.team === team);
+    // console.log(nascarObj);
+    
     pod.forEach(opponent => {
-      let oppPoints = resultsJSON[week][opponent];
-      let teamObj = team.find()
-      // TODO update OBJECT
-      // if (points > oppPoints) ranks[team].wins = ranks[team].wins + 1;
-      // if (oppPoints > points) ranks[team].losses = ranks[team].losses + 1;
-      // if (points === oppPoints) ranks[team].ties = ranks[team].ties + 1;
-      // ranks[team].pointsFor = ranks[team].pointsFor + points;
-      // ranks[team].pointsAgainst = ranks[team].pointsAgainst + oppPoints;
+      updateRanks(rankObj, points, opponent, week);
+    });
+    
+    nascarPod.forEach(opponent => {
+      updateRanks(nascarObj, points, opponent, week);
     })
   })
 });
 
-console.log(ranks);
+ranks.sort((a, b) => {
+  let aWins = a.wins + 0.5 * a.ties;
+  let bWins = a.wins + 0.5 * b.ties;
+  if (aWins === bWins) {
+    return a.pointsFor > b.pointsFor ? -1 : 1;
+  }
+  return aWins > bWins ? -1 : 1;
+});
+
+nascarRanks.sort((a, b) => {
+  let aWins = a.wins + 0.5 * a.ties;
+  let bWins = a.wins + 0.5 * b.ties;
+  if (aWins === bWins) {
+    return a.pointsFor > b.pointsFor ? -1 : 1;
+  }
+  return aWins > bWins ? -1 : 1;
+});
+
+ranks.forEach((team, i) => {
+  console.log(`${i + 1}: ${team.team} ${team.wins}-${team.losses}-${team.ties} -- ${team.pointsFor / (3 * weeks.length)} - ${team.pointsAgainst / (3 * weeks.length)}`);
+});
+
+nascarRanks.forEach((team, i) => {
+  console.log(`${i + 1}: ${team.team} ${team.wins}-${team.losses}-${team.ties} -- ${team.pointsFor / (3 * weeks.length)} - ${team.pointsAgainst / (3 * weeks.length)}`);
+});
+
+// TODO create export ranks
+// TODO extract into funtions
